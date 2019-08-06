8a5410ec06de9b320205dd9a8ca353de4b9f0e18
Till Hellmund <thellmund@gmail.com>
2019-08-06 21:56:39 +0200

    Remove unused libraries

--- app/src/main/java/com/hellmund/primetime/di/NetworkModule.kt
+++ app/src/main/java/com/hellmund/primetime/di/NetworkModule.kt
@@ -12,7 +12,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.threeten.bp.LocalDate
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -64,7 +63,6 @@ object NetworkModule {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService::class.java)
